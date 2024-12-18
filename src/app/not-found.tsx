e4730294c68bd8d63dc71bdace292168a197ac2b
pepeien <erickfcaldeira@gmail.com>
2024-12-17 22:53:53 -0300

    Updated not found page styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,21 +6,23 @@ import { getDictionary, getServerDefaultLocale } from '@dictionary';
 // Pages
 import { Layout } from '@pages';
 
+// Components
+import { Navigator } from '@components';
+
 export default async function Page() {
     const dictionary = await getDictionary(getServerDefaultLocale());
 
     return (
-        <Layout.generatePage params={{ lang: getServerDefaultLocale(), reference: '' }}>
-            <main className='not-found --page --hidden-overflow-all --fade-in'>
-                <section>
-                    <div className='not-found__title'>
-                        <div className='not-found__title__text --flex-column'>
-                            <h3>{dictionary['NOT_FOUND_TEXT']}</h3>
-                            <h2>404</h2>
-                        </div>
+        <Layout.generatePage params={{ lang: getServerDefaultLocale() }}>
+            <Navigator dictionary={dictionary} location='' />
+            <section className='not-found'>
+                <div className='not-found__title --flex-row'>
+                    <div className='not-found__title__text --flex-column'>
+                        <h3>{dictionary['NOT_FOUND_TEXT']}</h3>
+                        <h2>404</h2>
                     </div>
-                </section>
-            </main>
+                </div>
+            </section>
         </Layout.generatePage>
     );
 }
